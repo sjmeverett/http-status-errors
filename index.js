b2865d8899420eeb735a3b090ab54de397598d27
Stewart MacKenzie-Leigh <stewart@stugo.co.uk>
2016-10-20 13:47:11 +0100

    Such a numpty

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+var util = require('util');
+
 ([
   {name: 'BadRequestError', message: 'There is a problem with the submitted request.', status: 400},
   {name: 'NotAuthenticatedError', message: 'You are not authenticated.  Please authenticate and try again.', status: 401},
@@ -6,7 +8,7 @@
   {name: 'ForbiddenError', message: 'You are forbidden from accessing the requested resource.', status: 403},
   {name: 'NotFoundError', message: 'The requested resource was not found.', status: 404},
   {name: 'MethodNotAllowedError', message: 'The resource does not support the requested method.', status: 405},
-  {name: 'NotAcceptableError', message: 'The resource cannot be formatted according to the requested type in the Accept header.', status: 406},
+  {name: 'NotAcceptableError', message: 'The resource cannot be formatted according to the requested type in the Accept header', status: 406},
   {name: 'ConflictError', message: 'There is a conflict with the requested resource.', status: 409},
   {name: 'TeapotError', message: 'I\'m a teapot...', status: 418},
   {name: 'UnavailableError', message: 'The requested resource is unavailable for legal reasons.', status: 451},
